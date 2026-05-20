4f19bffd851e3715d6c94afa949f6c42e351477b
Stanislav Krizek <krizek@sycom.sk>
2026-05-20 22:19:29 +0200

    v3.14.1: edit komentarov + git verzia z tagu
    - Kazdy moze editovat vlastny komentar
    - ADMIN moze editovat vsetky komentare
    - Edit sa loguje do audit logu (EDIT_COMMENT)
    - Verzia portalu sa cita z git tagu automaticky pri builde

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: { NEXT_PUBLIC_APP_VERSION: require('./package.json').version },
+  env: { NEXT_PUBLIC_APP_VERSION: (() => { try { const { execSync } = require('child_process'); return execSync('git describe --tags --abbrev=0', {cwd: __dirname}).toString().trim().replace(/^v/, '') } catch { return require('./package.json').version } })() },
   images: {
     unoptimized: true,
     domains: ['portal.sycom.sk', 'sycom.sk'],
